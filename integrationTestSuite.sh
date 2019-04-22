6681439dbbfde193984478e16e8d5b8cae1eeaf3
Rishi Agrawal <rishi.b.agrawal@gmail.com>
2019-04-22 22:54:00 +0530

    Check the original curl

--- integrationTestSuite.sh
+++ integrationTestSuite.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
-curl --tlsv1 -k --header 'Accept: application/json' 'https://localhost:8080/v2/ping' -v
+#curl --tlsv1 -k --header 'Accept: application/json' 'https://localhost:8080/v2/ping' -v
+curl -X GET --header 'Accept: application/json' 'http://localhost:8080/v2/ping' -v
 set +x
