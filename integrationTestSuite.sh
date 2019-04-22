79c3976026f01bb58c0b374bfb4d82ab4cda48a4
Garima Agarwal <agarwalgarima90@gmail.com>
2019-04-22 22:42:26 +0530

    add -k

--- integrationTestSuite.sh
+++ integrationTestSuite.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-curl -X GET --header 'Accept: application/json' 'https://localhost:8080/v2/ping' -v
+curl -k --header 'Accept: application/json' 'http://localhost:8080/v2/ping' -v
 set +x
